9055cdc0274cc073b69084e63993cdbf18d61da7
gunu <kkw7654321@gmail.com>
2023-11-23 20:02:31 +0900

    add Header finished

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,9 +1,9 @@
 import React, { KeyboardEvent, ChangeEvent, useRef, useState, useEffect } from 'react';
 import './style.css';
-import { useNavigate, useParams } from 'react-router-dom';
-import { AUTH_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
 import { useCookies } from 'react-cookie';
-import { useLoginUserStore } from 'stores';
+import { useBoardStore, useLoginUserStore } from 'stores';
 
 //component
 export default function Header() {
@@ -12,6 +12,16 @@ export default function Header() {
   const [cookie, setCookie] = useCookies();
   const [isLogin, setLogin] = useState<boolean>(false);
   const {loginUser, setLoginUser, resetLoginUser} = useLoginUserStore();
+  
+  const {pathname} = useLocation();
+  const [isAuthPage, setAuthPage] = useState<boolean>(false);
+  const [isMainPage, setMainPage] = useState<boolean>(false);
+  const [isSearchPage, setSearchPage] = useState<boolean>(false);
+  const [isBoardDetailPage, setBoardDetailPage] = useState<boolean>(false);
+  const [isBoardWritePage, setBoardWritePage] = useState<boolean>(false);
+  const [isBoardUpdatePage, setBoardUpdatePage] = useState<boolean>(false);
+  const [isUserPage, setUserPage] = useState<boolean>(false);
+
 
   //function
   const navigate = useNavigate();
@@ -91,13 +101,46 @@ export default function Header() {
       navigate(AUTH_PATH());
     };
 
+    //render
     if(isLogin && userEmail===loginUser?.email)
     return(<div className='white-button' onClick={onSingOutButtonClickHandler}>{'로그아웃'}</div>);
     if(isLogin)
-    //render
     return(<div className='white-button' onClick={onMyPageButtonClickHandler}>{'마이페이지'}</div>);
     return(<div className='black-button' onClick={onSignInButtonClickHandler}>{'로그인'}</div>);
   }
+
+  //component 업로드버튼
+  const UploadButton = () => {
+    
+    //state
+    const {title, content, boardImageFileList, resetBoard} = useBoardStore();
+
+    //event Handler
+    const onUploadButtonClickHandler = () =>{
+
+    }
+    //render
+    if(title && content)
+    return(<div className='black-button' onClick={onUploadButtonClickHandler}>{'업로드'}</div>);
+    return(<div className='disable-button'>{'업로드'}</div>);
+  }
+  //effect
+  useEffect(() => {
+    const isAuthPage = pathname.startsWith(AUTH_PATH());
+    setAuthPage(isAuthPage);
+    const isMainPage = pathname === MAIN_PATH();
+    setMainPage(isMainPage);
+    const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+    setSearchPage(isSearchPage);
+    const isBoardDetailPage = pathname.startsWith(BOARD_PATH()+ '/' + BOARD_DETAIL_PATH(''));
+    setBoardDetailPage(isBoardDetailPage);
+    const isBoardWritePage = pathname.startsWith(BOARD_PATH()+ '/' + BOARD_WRITE_PATH());
+    setBoardWritePage(isBoardWritePage);
+    const isBoardUpdatePage = pathname.startsWith(BOARD_PATH()+ '/' + BOARD_UPDATE_PATH(''));
+    setBoardUpdatePage(isBoardUpdatePage);
+    const isUserPage = pathname.startsWith(USER_PATH(''));
+    setUserPage(isUserPage);
+  },[pathname]);
 
   //render:  Main
   return (
@@ -110,8 +153,9 @@ export default function Header() {
           <div className='header-logo'>{'kuns Board'}</div>
         </div>
         <div className='header-right-box'>
-          <SearchButton />
-          <MyPageButton />
+          {(isAuthPage || isMainPage || isSearchPage || isBoardDetailPage) && <SearchButton />}
+          {(isUserPage || isMainPage || isSearchPage || isBoardDetailPage) && <MyPageButton />}
+          {(isBoardWritePage || isBoardUpdatePage) && <UploadButton />}
         </div>
       </div>
     </div>
