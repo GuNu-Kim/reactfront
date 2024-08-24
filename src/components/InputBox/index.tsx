984c76f480d8edb29f2fe9ed4fc7bb4f8bfc33a5
gunu <kkw7654321@gmail.com>
2024-08-24 18:44:24 +0900

    수정사항2

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, Dispatch, forwardRef, KeyboardEvent, SetStateAction } from 'react';
+import React, { ChangeEvent, forwardRef, KeyboardEvent } from 'react';
 import './style.css';
 
 
@@ -9,9 +9,9 @@ interface Props{
     error: boolean;
     placeholder: string;
     value: string;
-    setValue: Dispatch<SetStateAction<string>>;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
     
-    icon?: string;
+    icon?: 'eye-light-off-icon' | 'eye-light-on-icon' | 'expand-right-light-icon';
     onButtonClick?: () => void;
 
     message?: string;
@@ -23,13 +23,9 @@ interface Props{
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
     
     //state
-    const {label, type, error, placeholder, value, setValue, icon, onButtonClick, message, onKeyDown} = props;
+    const {label, type, error, placeholder, value, onChange, icon, onButtonClick, message, onKeyDown} = props;
     
     //event handler
-    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-        const {value} = event.target;
-        setValue(value);
-    };
     const onKeyDownhandler = (event: KeyboardEvent<HTMLInputElement>) => {
         if(!onKeyDown) return;
         onKeyDown(event);
@@ -40,9 +36,9 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
         <div className='inputbox'>
             <div className='inputbox-label'>{label}</div>
             <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
-                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownhandler}/>
+                <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChange} onKeyDown={onKeyDownhandler}/>
                 {onButtonClick !== undefined && (
-                    <div className='icon-button'>
+                    <div className='icon-button' onClick={onButtonClick}>
                         {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
                     </div>
                 )}
